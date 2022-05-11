86820f27cb03bffb88c6c7bd58efd1205f16619d
meeran2019 <sheikmeeran.sj@gmail.com>
2022-05-11 22:16:13 +0530

    update

--- shell-code/getopts1.sh
+++ shell-code/getopts1.sh
@@ -1,19 +1,39 @@
 #!/bin/bash 
 
-while getopts ltr OPTIONS
+# while getopts 'lt:r:' OPTIONS
+# do 
+# echo "${OPTIONS}"
+#     if [[ $OPTIONS = 'l' ]]
+#     then 
+#         echo 'Length..'
+#         echo ${OPTARG}
+#     elif [[ $OPTIONS = 't' ]]
+#     then 
+#         echo 'Time..'
+#         echo ${OPTARG}
+#     else 
+#         echo "Reverse.."
+#         echo ${OPTARG}
+#     fi 
+# done 
+
+while getopts 'l:t:r:' OPTIONS
 do 
+echo $OPTIND 
 echo "${OPTIONS}"
-    if [[ $OPTIONS = 'l' ]]
-    then 
-        echo 'Length'
-    elif [[ $OPTIONS = 't' ]]
-    then 
+    case $OPTIONS in 
+    l)
+        echo 'Length..'
+        echo ${OPTARG}
+    ;;
+    t) 
         echo 'Time..'
-    else 
-        echo "invaid option.."
-    fi 
+        echo ${OPTARG}
+    ;;
+    r)
+        echo "Reverse.."
+        echo ${OPTARG}
+    ;;
+    esac 
 done 
 
-
-getopts ltr OPTIONS
-echo "New value is $OPTIONS"
